91b476cd381dafbdbd67e50ce86c43883c37d197
rafaquelhodev <48420402+rafaquelhodev@users.noreply.github.com>
2020-06-18 15:23:53 -0300

    admin front funcionalities

--- src/controllers/AdminController.go
+++ src/controllers/AdminController.go
@@ -12,18 +12,25 @@ type AdminController struct {
 	Routes []string
 }
 
-// Home login home page
+// Home is the admin home page
 func (t *AdminController) Home() {
 	t.Ctx.Template = "homeadmin"
 	t.HTML(http.StatusOK)
 }
 
-//NewFindInfectedControllerController returns a new FindInfectedController
+// DeclareInfection logs in at the admin home page
+func (t *AdminController) DeclareInfection() {
+	t.Ctx.Template = "admindeclareinfection"
+	t.HTML(http.StatusOK)
+}
+
+//NewAdminController is the controller for the admin
 // (get or post); url ; method
 func NewAdminController() controller.Controller {
 	return &AdminController{
 		Routes: []string{
 			"get;/admin;Home",
+			"get;/admin/addinfected;DeclareInfection",
 		},
 	}
 }
